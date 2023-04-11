e20d4b3db24b4079e0f38cd573cc4bb2722c804d
Lucas <lucaskreuch77@gmail.com>
2023-04-11 16:17:49 -0300

    AULA 1 | PSW 6.0

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.urls import reverse
+
 
 def cadastro(request):
     if request.method == "GET":
@@ -31,4 +32,22 @@ def cadastro(request):
 
         #messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso.')
         return redirect(reverse('login'))
+    
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect(reverse('login'))
+        
+        auth.login(request, user)
+        return redirect('/eventos/novo_evento/')
     
